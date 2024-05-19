8d75b75c785a05a6474387d6a0422e6cecca0133
Maksym Horak <maximgorak2005@gmail.com>
2024-05-19 13:11:20 +0300

    initialized frontend and backend folders

---
+++ backend/src/main/java/com/mhorak/lyrichunter/LyricHunterApplication.java
@@ -0,0 +1,13 @@
+package com.mhorak.lyrichunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricHunterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mhorak/lyrichunter/LyricHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhorak.lyrichunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
