675cf1738852620cf33ae8a49436fcde650c0b13
d3nnyyy <d.tsebulia@gmail.com>
2025-01-17 21:09:51 +0100

    changed url api to the deployed one

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const urlApi = "http://localhost:8080";
+// export const urlApi = "http://localhost:8080";
+export const urlApi = "https://lyrichunt-backend-bghsccfhd5azh7e3.polandcentral-01.azurewebsites.net";
